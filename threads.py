de8329c93582701f45ba4ebcc7b65706971de30c
Michal Jamroz <michal@yerbamate.com.pl>
2011-01-23 22:05:08 -0500

    added movie stream example

--- threads.py
+++ threads.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
-#public domain, jamroz AT chem.uw.edu.pl
-#script run stream in python threads - you can init few streams from one script
+#public domain, Michal Jamroz, jamroz AT chem.uw.edu.pl
+# REQUIRE "libshout binding for python". You can download source from icecast.org
+
+#script run stream in python threads - you can init few streams from this one script
 #additionaly, script plays jingles
 #and write 7 future songs to file "mountpoint-current.txt"
+
+#you can stream movie, too - check examples at bottom
 
 #CHANGE:
 hostname = "localhost"
 port = 8000
 password = "password"
-jingle_after = 4 #play jingle after 4 songs
+jingle_after = 4 #play jingle after each 4 songs
 jingles="/home/user/jingles" #directory with jingle files, if you dont have jingles - leave empty ""
 
 #defaults: bitrate 128, channels: 2, samplerate: 44100, music format: mp3 (can be "vorbis")
 #at the bottom of file you have examples
 
-# you need libshout binding to python. You can download source from icecast.org
 
 import shout
 import sys
@@ -23,16 +26,19 @@ from random import shuffle,choice
 import threading
 
 class RunStream (threading.Thread):
-   def __init__ (self, channel_mount, music_directory, station_url, genre,name, description, bitrate="128", samplerate="44100", channels="2",music_format="mp3",jingle_dir=""):
+   def __init__ (self, channel_mount, music_directory, station_url, genre,name, description, bitrate="128", samplerate="44100", channels="2",music_format="mp3",jingle_dir="",ogv=0):
       #init shout connection
       global hostname,port,password
       self.song_counter = 0
       self.s = shout.Shout()
       self.s.audio_info = {shout.SHOUT_AI_BITRATE:bitrate, shout.SHOUT_AI_SAMPLERATE:samplerate, shout.SHOUT_AI_CHANNELS:channels}
       self.s.name = name
+      self.ogv = ogv
       self.s.description = description
       self.s.host = hostname
       self.s.format = music_format # vorbis OR mp3
+      if(ogv==1):
+         self.s.format = vorbis
       self.s.port = port
       self.s.password = password
       self.s.mount = channel_mount
@@ -43,6 +49,15 @@ class RunStream (threading.Thread):
       self.music_directory = music_directory
       
       threading.Thread.__init__ (self)
+   def scan_directories_ogv(self):
+      if (len(self.jingle_dir)>1):
+         print "jingle dir: "+self.jingle_dir
+         self.jingle_files = glob(self.jingle_dir+"/*.[oO][gG][vV]")
+      # only 3-depth !!!!!!!!!!!!!!!!!!!
+      self.files_array = glob(self.music_directory+"/*.[oO][gG][vV]")  + glob(self.music_directory+"/*/*.[oO][gG][vV]") + glob(self.music_directory+"/*/*/*.[oO][gG][vV]")
+      print str(len(self.files_array))+" files"
+      shuffle(self.files_array) # random playlist, if you dont like - comment
+      
    def scan_directories(self):
       if (len(self.jingle_dir)>1):
          print "jingle dir: "+self.jingle_dir
@@ -55,14 +70,17 @@ class RunStream (threading.Thread):
    def run (self):
       global jingle_after
       while 1: #infinity
-         self.scan_directories() # rescan dir, maybe in time you add some new songs
-         self.song_counter = 0
+         if(self.ogv==0):
+            self.scan_directories() # rescan dir, maybe in time you add some new songs
+         else:
+            self.scan_directories_ogv() #scan movies
+         self.song_counter = 0   
          for e in self.files_array:
             self.write_future()
             self.sendfile(e)
             self.song_counter = self.song_counter + 1
             if(self.song_counter%jingle_after==0):
-               self.sendfile(choice(self.jingle_files)) # play random jingle
+               self.sendfile(choice(self.jingle_files)) # play random jingle/movieclip
          
    def format_songname(self,song): # format song name - on filename (strip "mp3", change _ to " ". You can add here some mp3 tag reader library
       result = song.split("/")[-1].split(".")
@@ -103,9 +121,17 @@ class RunStream (threading.Thread):
 # run stream with bitrate 128
 RunStream(channel_mount="/stream",music_directory="/home/user/mp3",station_url="http://music.com",genre="tekno, hardcore, tribe, mental",name="my radiostation",description="some desc",jingle_dir=jingles).start()
 
+
+# run another stream with bitrate 128
+RunStream(channel_mount="/stream23",music_directory="/home/user/mp3_classics",station_url="http://music.com",genre="tekno, hardcore, tribe, mental",name="my radiostation",description="some desc",jingle_dir=jingles).start()
+
+
 #run copy of stream with bitrate 64
 RunStream(channel_mount="/stream2",music_directory="/home/user/mp3",station_url="http://music.com",genre="tekno, hardcore, tribe, mental",name="my radiostation",description="some desc",jingle_dir=jingles,bitrate="64").start()
 
 #run same stream with bitrate 256
 RunStream(channel_mount="/stream3",music_directory="/home/user/mp3",station_url="http://music.com",genre="tekno, hardcore, tribe, mental",name="my radiostation",description="some desc",jingle_dir=jingles,bitrate="256").start()
 
+#run movie stream
+#playing ogv (theora format) movies from /home/user/ogv and advertisements from $HOME/adv
+RunStream(channel_mount="/television.ogv",music_directory="/home/user/ogv",station_url="http://tv.com",genre="melodrama",name="my tv station",description="some desc",jingle_dir="/home/user/adv",ogv=1).start()
